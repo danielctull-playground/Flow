cd2937e85a03b440d09f19d0096cbe17567c5701
Daniel Tull <dt@danieltull.co.uk>
2022-01-23 19:23:35 +0000

    Add Input parameter

--- Sources/Flow/Flow.swift
+++ Sources/Flow/Flow.swift
@@ -1,12 +1,19 @@
 
-public struct Flow<Output> {
+public struct Flow<Input, Output> {
 
-    private let task: () async throws -> Output
-    public init(_ task: @escaping () async throws -> Output) {
+    private let task: (Input) async throws -> Output
+    public init(_ task: @escaping (Input) async throws -> Output) {
         self.task = task
     }
 
+    public func callAsFunction(_ input: Input) async throws -> Output {
+        try await task(input)
+    }
+}
+
+extension Flow where Input == Void {
+
     public func callAsFunction() async throws -> Output {
-        try await task()
+        try await task(())
     }
 }
--- Tests/FlowTests/FlowTests.swift
+++ Tests/FlowTests/FlowTests.swift
@@ -4,10 +4,22 @@ import Flow
 
 final class FlowTests: XCTestCase {
 
-    func testPerform() async throws {
-        var didPerform = false
-        let flow = Flow { didPerform = true }
-        try await flow()
-        XCTAssert(didPerform)
+    func testInput() async throws {
+        var input: Int?
+        let flow = Flow { input = $0 }
+        try await flow(2)
+        XCTAssertEqual(input, 2)
+    }
+
+    func testOutput() async throws {
+        let flow = Flow { "output" }
+        let output = try await flow()
+        XCTAssertEqual(output, "output")
+    }
+
+    func testInputOutput() async throws {
+        let flow = Flow<Int, Int> { 2 * $0 }
+        let output = try await flow(2)
+        XCTAssertEqual(output, 4)
     }
 }
