f425c78420ee1ee7b9ff3cdaefb48b55353c5752
Daniel Tull <dt@danieltull.co.uk>
2022-01-23 19:12:35 +0000

    Create package

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "Flow",
+    products: [
+        .library(name: "Flow", targets: ["Flow"]),
+    ],
+    targets: [
+        .target(
+            name: "Flow"),
+        .testTarget(
+            name: "FlowTests",
+            dependencies: ["Flow"]),
+    ]
+)
---
+++ Sources/Flow/Flow.swift
@@ -0,0 +1,7 @@
+
+public struct Flow {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
