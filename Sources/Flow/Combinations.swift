4f477472df48ac30e06a14d6311293fa1b44067e
Daniel Tull <dt@danieltull.co.uk>
2022-01-26 12:06:09 +0000

    Add variants of merge for up to eight flows
    This matches the amount in Combine for merging publishers.

--- Sources/Flow/Combinations.swift
+++ Sources/Flow/Combinations.swift
@@ -1,13 +1,116 @@
 
 extension Flow {
 
-    public func merge<A>(
-        with flowA: Flow<A>
-    ) -> Flow<(Output, A)> {
-        Flow<(Output, A)> {
+    public func merge<B>(
+        with flowB: Flow<B>
+    ) -> Flow<(Output, B)> {
+        Flow<(Output, B)> {
             async let output = self()
-            async let a = flowA()
-            return try await (output, a)
+            async let b = flowB()
+            return try await (output, b)
+        }
+    }
+
+    public func merge<B, C>(
+        with flowB: Flow<B>,
+        _ flowC: Flow<C>
+    ) -> Flow<(Output, B, C)> {
+        Flow<(Output, B, C)> {
+            async let output = self()
+            async let b = flowB()
+            async let c = flowC()
+            return try await (output, b, c)
+        }
+    }
+
+    public func merge<B, C, D>(
+        with flowB: Flow<B>,
+        _ flowC: Flow<C>,
+        _ flowD: Flow<D>
+    ) -> Flow<(Output, B, C, D)> {
+        Flow<(Output, B, C, D)> {
+            async let output = self()
+            async let b = flowB()
+            async let c = flowC()
+            async let d = flowD()
+            return try await (output, b, c, d)
+        }
+    }
+
+    public func merge<B, C, D, E>(
+        with flowB: Flow<B>,
+        _ flowC: Flow<C>,
+        _ flowD: Flow<D>,
+        _ flowE: Flow<E>
+    ) -> Flow<(Output, B, C, D, E)> {
+        Flow<(Output, B, C, D, E)> {
+            async let output = self()
+            async let b = flowB()
+            async let c = flowC()
+            async let d = flowD()
+            async let e = flowE()
+            return try await (output, b, c, d, e)
+        }
+    }
+
+    public func merge<B, C, D, E, F>(
+        with flowB: Flow<B>,
+        _ flowC: Flow<C>,
+        _ flowD: Flow<D>,
+        _ flowE: Flow<E>,
+        _ flowF: Flow<F>
+    ) -> Flow<(Output, B, C, D, E, F)> {
+        Flow<(Output, B, C, D, E, F)> {
+            async let output = self()
+            async let b = flowB()
+            async let c = flowC()
+            async let d = flowD()
+            async let e = flowE()
+            async let f = flowF()
+            return try await (output, b, c, d, e, f)
+        }
+    }
+
+    public func merge<B, C, D, E, F, G>(
+        with flowB: Flow<B>,
+        _ flowC: Flow<C>,
+        _ flowD: Flow<D>,
+        _ flowE: Flow<E>,
+        _ flowF: Flow<F>,
+        _ flowG: Flow<G>
+    ) -> Flow<(Output, B, C, D, E, F, G)> {
+        Flow<(Output, B, C, D, E, F, G)> {
+            async let output = self()
+            async let b = flowB()
+            async let c = flowC()
+            async let d = flowD()
+            async let e = flowE()
+            async let f = flowF()
+            async let g = flowG()
+            return try await (output, b, c, d, e, f, g)
+        }
+    }
+
+
+    public func merge<B, C, D, E, F, G, H>(
+        with flowB: Flow<B>,
+        _ flowC: Flow<C>,
+        _ flowD: Flow<D>,
+        _ flowE: Flow<E>,
+        _ flowF: Flow<F>,
+        _ flowG: Flow<G>,
+        _ flowH: Flow<H>
+    ) -> Flow<(Output, B, C, D, E, F, G, H)> {
+        Flow<(Output, B, C, D, E, F, G, H)> {
+            async let output = self()
+            async let b = flowB()
+            async let c = flowC()
+            async let d = flowD()
+            async let e = flowE()
+            async let f = flowF()
+            async let g = flowG()
+            async let h = flowH()
+            return try await (output, b, c, d, e, f, g, h)
         }
     }
 }
--- Tests/FlowTests/CombinationTests.swift
+++ Tests/FlowTests/CombinationTests.swift
@@ -4,11 +4,80 @@ import Flow
 
 final class CombinationTests: XCTestCase {
 
-    func testMerge() async throws {
-        let flow = Flow.just(4)
-            .merge(with: .just("A"))
+    func testMerge2() async throws {
+        let flow = Flow.just("A")
+            .merge(with: .just("B"))
         let output = try await flow()
-        XCTAssertEqual(output.0, 4)
-        XCTAssertEqual(output.1, "A")
+        XCTAssertEqual(output.0, "A")
+        XCTAssertEqual(output.1, "B")
+    }
+
+    func testMerge3() async throws {
+        let flow = Flow.just("A")
+            .merge(with: .just("B"), .just("C"))
+        let output = try await flow()
+        XCTAssertEqual(output.0, "A")
+        XCTAssertEqual(output.1, "B")
+        XCTAssertEqual(output.2, "C")
+    }
+
+    func testMerge4() async throws {
+        let flow = Flow.just("A")
+            .merge(with: .just("B"), .just("C"), .just("D"))
+        let output = try await flow()
+        XCTAssertEqual(output.0, "A")
+        XCTAssertEqual(output.1, "B")
+        XCTAssertEqual(output.2, "C")
+        XCTAssertEqual(output.3, "D")
+    }
+
+    func testMerge5() async throws {
+        let flow = Flow.just("A")
+            .merge(with: .just("B"), .just("C"), .just("D"), .just("E"))
+        let output = try await flow()
+        XCTAssertEqual(output.0, "A")
+        XCTAssertEqual(output.1, "B")
+        XCTAssertEqual(output.2, "C")
+        XCTAssertEqual(output.3, "D")
+        XCTAssertEqual(output.4, "E")
+    }
+
+    func testMerge6() async throws {
+        let flow = Flow.just("A")
+            .merge(with: .just("B"), .just("C"), .just("D"), .just("E"), .just("F"))
+        let output = try await flow()
+        XCTAssertEqual(output.0, "A")
+        XCTAssertEqual(output.1, "B")
+        XCTAssertEqual(output.2, "C")
+        XCTAssertEqual(output.3, "D")
+        XCTAssertEqual(output.4, "E")
+        XCTAssertEqual(output.5, "F")
+    }
+
+    func testMerge7() async throws {
+        let flow = Flow.just("A")
+            .merge(with: .just("B"), .just("C"), .just("D"), .just("E"), .just("F"), .just("G"))
+        let output = try await flow()
+        XCTAssertEqual(output.0, "A")
+        XCTAssertEqual(output.1, "B")
+        XCTAssertEqual(output.2, "C")
+        XCTAssertEqual(output.3, "D")
+        XCTAssertEqual(output.4, "E")
+        XCTAssertEqual(output.5, "F")
+        XCTAssertEqual(output.6, "G")
+    }
+
+    func testMerge8() async throws {
+        let flow = Flow.just("A")
+            .merge(with: .just("B"), .just("C"), .just("D"), .just("E"), .just("F"), .just("G"), .just("H"))
+        let output = try await flow()
+        XCTAssertEqual(output.0, "A")
+        XCTAssertEqual(output.1, "B")
+        XCTAssertEqual(output.2, "C")
+        XCTAssertEqual(output.3, "D")
+        XCTAssertEqual(output.4, "E")
+        XCTAssertEqual(output.5, "F")
+        XCTAssertEqual(output.6, "G")
+        XCTAssertEqual(output.7, "H")
     }
 }
