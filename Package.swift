89ebff9e834ee44a368f3a16ff5ef3006e638db1
Daniel Tull <dt@danieltull.co.uk>
2022-02-02 17:58:18 +0000

    Add other platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,11 @@ import PackageDescription
 let package = Package(
     name: "Flow",
     platforms: [
+        .iOS(.v13),
+        .macCatalyst(.v13),
         .macOS(.v12),
+        .tvOS(.v13),
+        .watchOS(.v7),
     ],
     products: [
         .library(name: "Flow", targets: ["Flow"]),
