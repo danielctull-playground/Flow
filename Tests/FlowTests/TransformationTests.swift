3805b72ad4be56af872869a6ac353aabaa4533fa
Daniel Tull <dt@danieltull.co.uk>
2022-04-03 10:05:14 +0100

    Use AssertOutput to increase clarity of tests

--- Tests/FlowTests/TransformationTests.swift
+++ Tests/FlowTests/TransformationTests.swift
@@ -5,18 +5,18 @@ import Flow
 final class TransformationTests: XCTestCase {
 
     func testMap() async throws {
-        let flow = Flow.just(4)
-            .map(String.init)
-        let output = try await flow()
-        XCTAssertEqual(output, "4")
+        await AssertOutput("4") {
+            Flow.just(4)
+                .map(String.init)
+        }
     }
 
     func testFlatMap() async throws {
-        let flow = Flow.just(4)
-            .flatMap { value in
-                Flow { String(value) }
-            }
-        let output = try await flow()
-        XCTAssertEqual(output, "4")
+        await AssertOutput("4") {
+            Flow.just(4)
+                .flatMap { value in
+                    Flow { String(value) }
+                }
+        }
     }
 }
